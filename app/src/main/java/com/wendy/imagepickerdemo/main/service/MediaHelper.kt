d06aeb2ca71aa9d3fe0f6611faeb6cbc214a1c51
wendyyanto <yanto.wendy98@gmail.com>
2020-04-27 17:48:39 +0700

    Add try catch when fetch image

--- app/src/main/java/com/wendy/imagepickerdemo/main/service/MediaHelper.kt
+++ app/src/main/java/com/wendy/imagepickerdemo/main/service/MediaHelper.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.database.Cursor
 import android.net.Uri
 import android.provider.MediaStore
+import android.util.Log
 import com.wendy.imagepickerdemo.main.model.ImageGalleryUiModel
 
 class MediaHelper {
@@ -23,32 +24,37 @@ class MediaHelper {
             val images: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
             val cursor: Cursor? =
                 context.contentResolver.query(images, projection, null, null, null)
-            
-            if (cursor?.moveToFirst() == true) {
-                var bucket: String
-                var uri: String
-                val bucketColumn: Int = cursor.getColumnIndex(projection[0])
-                val imageUriColumn: Int = cursor.getColumnIndex(projection[1])
 
-                do {
-                    bucket = cursor.getString(bucketColumn)
-                    uri = cursor.getString(imageUriColumn)
-                    if (fetchImageGalleryList[bucket] == null) {
-                        fetchImageGalleryList[bucket] = ArrayList()
-                    }
-                    if (uri.substring(
-                            uri.length - 3,
-                            uri.length
-                        ) in ALLOWED_IMAGE_TYPE || uri.substring(
-                            uri.length - 4,
-                            uri.length
-                        ) in ALLOWED_IMAGE_TYPE
-                    ) {
-                        fetchImageGalleryList[bucket]?.add(ImageGalleryUiModel(imageUri = uri))
-                    }
-                } while (cursor.moveToNext())
+            try {
+                if (cursor?.moveToFirst() == true) {
+                    var bucket: String
+                    var uri: String
+                    val bucketColumn: Int = cursor.getColumnIndex(projection[0])
+                    val imageUriColumn: Int = cursor.getColumnIndex(projection[1])
+
+                    do {
+                        bucket = cursor.getString(bucketColumn)
+                        uri = cursor.getString(imageUriColumn)
+                        if (fetchImageGalleryList[bucket] == null) {
+                            fetchImageGalleryList[bucket] = ArrayList()
+                        }
+                        if (uri.substring(
+                                uri.length - 3,
+                                uri.length
+                            ) in ALLOWED_IMAGE_TYPE || uri.substring(
+                                uri.length - 4,
+                                uri.length
+                            ) in ALLOWED_IMAGE_TYPE
+                        ) {
+                            fetchImageGalleryList[bucket]?.add(ImageGalleryUiModel(imageUri = uri))
+                        }
+                    } while (cursor.moveToNext())
+                }
+            } catch (e: Exception) {
+                Log.e(MediaHelper::javaClass.name, e.message)
+            } finally {
+                cursor?.close()
             }
-            cursor?.close()
 
             return fetchImageGalleryList
         }
