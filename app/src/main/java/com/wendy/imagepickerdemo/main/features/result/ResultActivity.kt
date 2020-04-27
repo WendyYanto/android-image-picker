8524e055b0931366f9045a14a1d6ac71a54a96ff
wendyyanto <yanto.wendy98@gmail.com>
2020-04-27 21:24:48 +0700

    Implements ListAdapter at ResultAdapter

--- app/src/main/java/com/wendy/imagepickerdemo/main/features/result/ResultActivity.kt
+++ app/src/main/java/com/wendy/imagepickerdemo/main/features/result/ResultActivity.kt
@@ -14,11 +14,18 @@ class ResultActivity : AppCompatActivity() {
         const val GET_IMAGE_GALLERY_RESULT = "GET_IMAGE_GALLERY_RESULT"
     }
 
+    private lateinit var imageGalleryResultAdapter: ImageGalleryResultAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityResultBinding = DataBindingUtil.setContentView(this, R.layout.activity_result)
-        val imageResultList = intent.getStringArrayListExtra(GET_IMAGE_GALLERY_RESULT)
-        binding.rvImageResult.layoutManager = LinearLayoutManager(this)
-        binding.rvImageResult.adapter = ImageGalleryResultAdapter(imageResultList)
+        val binding: ActivityResultBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_result)
+        val results = intent.getStringArrayListExtra(GET_IMAGE_GALLERY_RESULT)
+        imageGalleryResultAdapter = ImageGalleryResultAdapter()
+        with(binding.rvImageResult) {
+            layoutManager = LinearLayoutManager(context)
+            adapter = imageGalleryResultAdapter
+        }
+        imageGalleryResultAdapter.submitList(results)
     }
 }
--- app/src/main/java/com/wendy/imagepickerdemo/main/features/result/adapter/ImageGalleryResultAdapter.kt
+++ app/src/main/java/com/wendy/imagepickerdemo/main/features/result/adapter/ImageGalleryResultAdapter.kt
@@ -1,6 +1,8 @@
 package com.wendy.imagepickerdemo.main.features.result.adapter
 
 import android.databinding.DataBindingUtil
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -10,21 +12,17 @@ import com.bumptech.glide.Glide
 import com.wendy.imagepickerdemo.R
 import com.wendy.imagepickerdemo.databinding.ImagePickerItemsBinding
 
-class ImageGalleryResultAdapter(private val items: ArrayList<String>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val imageResultItem = LayoutInflater.from(parent.context).inflate(R.layout.image_picker_items, parent, false)
+class ImageGalleryResultAdapter :
+    ListAdapter<String, ImageGalleryResultAdapter.ImageResultViewHolder>(DIFF_UTIL) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageResultViewHolder {
+        val imageResultItem =
+            LayoutInflater.from(parent.context).inflate(R.layout.image_picker_items, parent, false)
         return ImageResultViewHolder(imageResultItem)
     }
 
-    override fun getItemCount(): Int {
-        return items.size
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val imageGalleryViewHolder = holder as ImageResultViewHolder
-        val imageUri = items[position]
-        imageGalleryViewHolder.bind(imageUri)
+    override fun onBindViewHolder(holder: ImageResultViewHolder, position: Int) {
+        val imageUri = getItem(position)
+        holder.bind(imageUri)
     }
 
     inner class ImageResultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -35,9 +33,21 @@ class ImageGalleryResultAdapter(private val items: ArrayList<String>) :
             viewBinding?.cbImageItem?.visibility = View.GONE
             viewBinding?.ivPhoto?.let {
                 it.scaleType = ImageView.ScaleType.FIT_CENTER
-                Glide.with(itemView).load(imageUri).placeholder(R.drawable.image_placeholder).into(it)
+                Glide.with(itemView).load(imageUri).placeholder(R.drawable.image_placeholder)
+                    .into(it)
             }
         }
+    }
 
+    companion object {
+        private val DIFF_UTIL = object : DiffUtil.ItemCallback<String>() {
+            override fun areItemsTheSame(old: String, new: String): Boolean {
+                return old == new
+            }
+
+            override fun areContentsTheSame(old: String, new: String): Boolean {
+                return old == new
+            }
+        }
     }
 }
